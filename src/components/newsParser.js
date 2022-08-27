2ff49b48039fe6ead3d9e73077eeb3d67e5ba462
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-08-27 21:21:56 +0400

    a

--- src/components/newsParser.js
+++ src/components/newsParser.js
@@ -3,35 +3,55 @@ function newsParser (){
     const fs = require ('fs');
     const buff = [];
 
-    function unwrapdiv (html) {
-        const REGEX = /<div class="contentpaneopen_fp clearfix">(.*?)<div class="createdate">(.*?)<\/div><\/div>/gms;
-        return [...html.matchAll( REGEX )];
+    function firstUnwrapFunc ( html ){
+        // эта функция анврапает table
+        const REGEXP = /<table class="blog_fp" cellpadding="0" cellspacing="0">(.*?)<\/table>/gms;
+        return [...html.matchAll( REGEXP)];
     }
 
-    function unwrapPreview( html ){
-        const REGEX = /<a href="(?<link>.*?)" class="contentpagetitle_fp">(?<title>.*?)<\/a>/gms;
-        const arr = [];
+    function secondUnwrapFunc ( html ){
+        // эта функция удаляет комментарии
+        const REGEXP = /<!--(.*?)-->/gms;
+        return html.replaceAll( /<!--(.*?)-->/gms , '').trim();
 
-        for (let match of [...html.matchAll( REGEX )]) {
-
-            const link = match.groups.link;
-            const title = match.groups.title;
-
-            arr.push({
-                link: link.replaceAll( /class..*/gms, '').trim(),
-                title: title.replaceAll( /\\\S/gms, '' ).trim()
-            })
-        }
-        return arr;
     }
+
+    function thirdUnwrapFunc ( html ){
+
+        const REGEXP = /<h2 class="contentheading_fp">.*?<a href="(?<pageHref>.*?)" class="contentpagetitle_fp">(?<title>.*?)<\/a>.*?<\/h2>.*?<div class="content-desc">.*?<img.*?src="(?<imgHref>.*?)"/gms;
+
+    }
+
+    // function unwrapdiv (html) {
+    //     const REGEX = /<div class="contentpaneopen_fp clearfix">(.*?)<div class="createdate">(.*?)<\/div><\/div>/gms;
+    //     return [...html.matchAll( REGEX )];
+    // }
+    //
+    // function unwrapPreview( html ){
+    //     const REGEX = /<a href="(?<link>.*?)" class="contentpagetitle_fp">(?<title>.*?)<\/a>/gms;
+    //     const arr = [];
+    //
+    //     for (let match of [...html.matchAll( REGEX )]) {
+    //
+    //         const link = match.groups.link;
+    //         const title = match.groups.title;
+    //
+    //         arr.push({
+    //             link: link.replaceAll( /class..*/gms, '').trim(),
+    //             title: title.replaceAll( /\\\S/gms, '' ).trim()
+    //         })
+    //     }
+    //     return arr;
+    // }
 
     (async () => {
         const request = await fetch('https://ks.psuti.ru/');
         const page = await request.text();
-        const firstUnwrap = unwrapdiv( page ) [0][0];
-        const secondUnwrap = unwrapPreview( firstUnwrap );
-        console.log(JSON.stringify(secondUnwrap));
-        fs.writeFileSync('../jsonFiles/secondUnwrap.txt', JSON.stringify(secondUnwrap, null, '\t'));
+        const firstUnwrap = firstUnwrapFunc( page )[0][0];
+        const secondUnwrap = secondUnwrapFunc( firstUnwrap );
+        // const secondUnwrap = unwrapPreview( firstUnwrap );
+        // console.log(JSON.stringify(secondUnwrap));
+        fs.writeFileSync('../jsonFiles/secondUnwrap.txt', secondUnwrap.toString());
     })();
 
     /*
